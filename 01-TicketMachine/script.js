350c825308b3f57910d90fe81ce3de7fa7a0c466
Gábor <gabor.varga@suprasoft.hu>
2022-05-17 09:49:00 +0200

    start

---
+++ 01-TicketMachine/script.js
@@ -0,0 +1,16 @@
+/*
+    Input:
+        How many tickets do you want?
+        What kind of ticket do you want?
+
+        Student ticket: 300 HUF
+        Adult ticket: 350 HUF
+
+        10% discount for purchases of more than 10 tickets
+*/
+
+var quantity = prompt('How many tickets do you want? Give me the quantity, please!')
+var typeOfTicket = prompt('What kind of ticket do you want? Student | Adult')
+
+alert(quantity);
+alert(typeOfTicket);
