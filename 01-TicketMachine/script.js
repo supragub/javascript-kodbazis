cacdc4445cbf699371e4f7e9d1f615c25f2df489
Gábor <gabor.varga@suprasoft.hu>
2022-05-17 13:09:42 +0200

    v1

--- 01-TicketMachine/script.js
+++ 01-TicketMachine/script.js
@@ -1,18 +1,26 @@
 /*
     Input:
-        How many tickets do you want?
-        What kind of ticket do you want?
+        - How many tickets do you want?
+        - What kind of ticket do you want?
 
         Student ticket: 300 HUF
         Adult ticket: 350 HUF
+        Pensioner ticket: 280 HUF
 
         10% discount for purchases of more than 10 tickets
 */
 
 var quantityOfTicket = prompt('How many tickets would you like to buy? Give me the quantity, please!')
-var typeOfTicket = prompt('What kind of ticket do you want? student | adult')
-var priceOfTicket = typeOfTicket === 'student' ? 300 : 350;
-var discount = quantityOfTicket > 10 ? 0.9 : 1;
-var TotalPrice = quantityOfTicket * priceOfTicket * discount
+var typeOfTicket = prompt('What kind of ticket do you want? student | adult | pensioner')
 
-alert("Total price: " + TotalPrice + " HUF");
+var price = {
+    student : 300,
+    adult : 350,
+    pensioner : 280,
+}
+
+var discount = quantityOfTicket > 10 ? 0.9 : 1;
+
+var totalPrice = quantityOfTicket * price[typeOfTicket] * discount
+
+alert("Total price: " + totalPrice + " HUF");
