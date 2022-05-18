b4ff2373108ebb74d9fec151990abcf0ec9555ee
Gábor <gabor.varga@suprasoft.hu>
2022-05-18 16:01:58 +0200

    v2 - input validation, price object

--- 01-TicketMachine/script.js
+++ 01-TicketMachine/script.js
@@ -1,26 +1,37 @@
 /*
     Input:
-        - How many tickets do you want?
-        - What kind of ticket do you want?
+        - How many tickets do you want? (prompt)
+        - What kind of ticket do you want? (prompt)
 
         Student ticket: 300 HUF
         Adult ticket: 350 HUF
         Pensioner ticket: 280 HUF
 
         10% discount for purchases of more than 10 tickets
+
+        Validation for quantity & type of ticket 
 */
 
-var quantityOfTicket = prompt('How many tickets would you like to buy? Give me the quantity, please!')
-var typeOfTicket = prompt('What kind of ticket do you want? student | adult | pensioner')
+var quantityOfTicket = prompt('How many tickets would you like to buy? Give me the quantity, please!');
+
+while (!quantityOfTicket.match(/^\d+/)) {
+    var quantityOfTicket = prompt('Wrong format! Give me a number, please!'); 
+}
+
+var typeOfTicket = prompt('What kind of ticket do you want? student | adult | pensioner');
+
+while (typeOfTicket != 'student' && typeOfTicket != 'adult' && typeOfTicket != 'pensioner') {
+    var typeOfTicket = prompt('Wrong type of ticket! Options: student | adult | pensioner');
+}
 
 var price = {
     student : 300,
     adult : 350,
     pensioner : 280,
-}
+};
 
 var discount = quantityOfTicket > 10 ? 0.9 : 1;
 
-var totalPrice = quantityOfTicket * price[typeOfTicket] * discount
+var totalPrice = quantityOfTicket * price[typeOfTicket] * discount;
 
 alert("Total price: " + totalPrice + " HUF");
